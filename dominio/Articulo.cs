ae03c0bffea56aa66afbe5ac79b975fd7c660b22
JuanEspinoza8 <juanespinoza.unco@gmail.com>
2025-01-02 16:44:45 -0300

    Agregar archivos de proyecto.

---
+++ dominio/Articulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int codigo { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public Marca marca { get; set; }
+
+        public Categoria categoria { get; set; }
+
+        public string imagen { get; set; }
+
+        public float precio { get; set; }
+
+    }
+}
