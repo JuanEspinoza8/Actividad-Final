85b578fcce2f51399e09b3f793f7ee6485230f50
JuanEspinoza8 <juanespinoza.unco@gmail.com>
2025-01-03 22:51:46 -0300

    Todo listo para esperar a don lucho again

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.dgvNegocio = new System.Windows.Forms.DataGridView();
             this.pbxArticulos = new System.Windows.Forms.PictureBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvNegocio)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxArticulos)).BeginInit();
             this.SuspendLayout();
@@ -37,28 +38,40 @@
             // dgvNegocio
             // 
             this.dgvNegocio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvNegocio.Location = new System.Drawing.Point(82, 30);
+            this.dgvNegocio.Location = new System.Drawing.Point(12, 12);
             this.dgvNegocio.Name = "dgvNegocio";
-            this.dgvNegocio.Size = new System.Drawing.Size(629, 374);
+            this.dgvNegocio.Size = new System.Drawing.Size(643, 374);
             this.dgvNegocio.TabIndex = 0;
             this.dgvNegocio.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             this.dgvNegocio.SelectionChanged += new System.EventHandler(this.dgvNegocio_SelectionChanged);
             // 
             // pbxArticulos
             // 
-            this.pbxArticulos.Location = new System.Drawing.Point(717, 30);
+            this.pbxArticulos.Location = new System.Drawing.Point(661, 12);
             this.pbxArticulos.Name = "pbxArticulos";
-            this.pbxArticulos.Size = new System.Drawing.Size(321, 374);
+            this.pbxArticulos.Size = new System.Drawing.Size(192, 374);
             this.pbxArticulos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxArticulos.TabIndex = 1;
             this.pbxArticulos.TabStop = false;
             this.pbxArticulos.Click += new System.EventHandler(this.pbxArticulos_Click);
             // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAgregar.Location = new System.Drawing.Point(12, 392);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1064, 443);
+            this.ClientSize = new System.Drawing.Size(861, 443);
+            this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.pbxArticulos);
             this.Controls.Add(this.dgvNegocio);
             this.Name = "Form1";
@@ -74,6 +87,7 @@
 
         private System.Windows.Forms.DataGridView dgvNegocio;
         private System.Windows.Forms.PictureBox pbxArticulos;
+        private System.Windows.Forms.Button btnAgregar;
     }
 }
 
---
+++ negocio/GenericoNegocio.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using accesoDatos;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class GenericoNegocio
+    {
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+            //SqlConnection conexion = new SqlConnection();
+            //SqlCommand comando = new SqlCommand();
+            //SqlDataReader lector;
+
+                Acceso datos = new Acceso();
+
+
+            try
+            {
+                //conexion.ConnectionString = "server=.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true";
+                //comando.CommandType = System.Data.CommandType.Text;
+                //comando.CommandText = "Select Codigo, Nombre, A.Descripcion, ImagenUrl, M.Descripcion Marca, C.Descripcion Categoria, Precio \r\nFrom ARTICULOS A, CATEGORIAS C, MARCAS M\r\nWhere A.IdMarca = M.Id AND A.IdCategoria = C.Id";
+                //comando.Connection = conexion;
+
+                //conexion.Open();
+                //lector = comando.ExecuteReader();
+                datos.setearConsulta("Select Codigo, Nombre, A.Descripcion, ImagenUrl, M.Descripcion Marca, C.Descripcion Categoria, Precio \r\nFrom ARTICULOS A, CATEGORIAS C, MARCAS M\r\nWhere A.IdMarca = M.Id AND A.IdCategoria = C.Id");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Codigo = (string)datos.Lector["Codigo"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Descripcion = (string)datos.Lector["Descripcion"];
+                    aux.Imagen = (string)datos.Lector["ImagenUrl"];
+                    aux.Marca = new Marca();
+                    aux.Marca.Descripcion = (string)datos.Lector["Marca"];
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Descripcion = (string)datos.Lector["Categoria"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+
+                    lista.Add(aux);
+
+                }
+
+                datos.cerrarConexion();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+            
+            
+        }
+    }
+}
